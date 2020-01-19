943204431a233ee1883da7d88f9343d08c05b898
Carlos Filoteo <c.filoteo12@gmail.com>
2020-01-19 15:11:21 -0700

    Add demo rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,36 +4,74 @@ import babel from "rollup-plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import serve from "rollup-plugin-serve";
 import livereload from "rollup-plugin-livereload";
+import htmlTemplate from "rollup-plugin-generate-html-template";
+import postcss from "rollup-plugin-postcss";
 import filesize from "rollup-plugin-filesize";
 
 const prod = process.env.NODE_ENV === "production";
 
-export default {
-  input: "src/index.js",
-  output: [
-    {
-      name: "ReactEmojiPickr",
-      file: pkg.browser,
+export default [
+  {
+    input: "src/index.js",
+    output: [
+      {
+        name: "ReactEmojiPickr",
+        file: pkg.browser,
+        format: "umd",
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM"
+        }
+      },
+      {
+        file: pkg.module,
+        format: "es"
+      }
+    ],
+    external: ["react", "react-dom"],
+    plugins: [
+      resolve(),
+      babel({
+        exclude: "node_modules/**"
+      }),
+      postcss({
+        modules: true
+      }),
+      prod && terser(),
+      prod && filesize()
+      // !prod && livereload(),
+      // !prod && serve()
+    ]
+  },
+  {
+    input: "demo/src/index.js",
+    output: {
+      file: "demo/index.js",
       format: "umd",
       globals: {
         react: "React",
         "react-dom": "ReactDOM"
       }
     },
-    {
-      file: pkg.module,
-      format: "es"
-    }
-  ],
-  external: ["react", "react-dom"],
-  plugins: [
-    resolve(),
-    babel({
-      exclude: "node_modules/**"
-    }),
-    prod && terser(),
-    prod && filesize(),
-    !prod && livereload(),
-    !prod && serve()
-  ]
-};
+    external: ["react", "react-dom"],
+    plugins: [
+      resolve({
+        customResolveOptions: {
+          moduleDirectory: "dist"
+        }
+      }),
+      babel({
+        exclude: "node_modules/**"
+      }),
+      htmlTemplate({
+        template: "demo/src/index.html",
+        target: "demo/index.html"
+      }),
+      postcss({
+        modules: true
+      }),
+      !prod && serve("demo"),
+      !prod && livereload(["dist", "demo"])
+    ]
+  }
+];
