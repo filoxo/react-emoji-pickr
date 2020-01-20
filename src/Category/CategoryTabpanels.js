449e32887ca0851925b92f9eb11aca95b4f11e09
Carlos Filoteo <c.filoteo12@gmail.com>
2020-01-20 13:06:59 -0700

    Render all Emoji

--- src/Category/CategoryTabpanels.js
+++ src/Category/CategoryTabpanels.js
@@ -2,10 +2,21 @@ import React from "react";
 import Tabs from "../Tabs/";
 import CATEGORIES from "./Categories";
 
+import Emojis from "../Emoji/Emojis";
+import EmojiButton from "../Emoji/Button";
+
 export default function CategoryTabpanels(props) {
   return (
     <div>
-      <Tabs.Tabpanel id={CATEGORIES.ALL}>All content</Tabs.Tabpanel>
+      <Tabs.Tabpanel id={CATEGORIES.ALL} data-emoji-list>
+        {Emojis.map(emoji => (
+          <EmojiButton
+            key={emoji.e}
+            emoji={emoji.e}
+            aria-label={emoji.n.join(" ")}
+          />
+        ))}
+      </Tabs.Tabpanel>
       <Tabs.Tabpanel id={CATEGORIES.FACES}>Faces content</Tabs.Tabpanel>
       <Tabs.Tabpanel id={CATEGORIES.ANIMALS}>Animals content</Tabs.Tabpanel>
       <Tabs.Tabpanel id={CATEGORIES.FOODS}>Foods content</Tabs.Tabpanel>
