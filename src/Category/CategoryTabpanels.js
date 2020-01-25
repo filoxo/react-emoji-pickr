168526276cabb90fd1c8990f6b972a8d3c1980ff
Carlos Filoteo <c.filoteo12@gmail.com>
2020-01-24 23:02:24 -0700

    Render emoji in Tabpanels

--- src/Category/CategoryTabpanels.js
+++ src/Category/CategoryTabpanels.js
@@ -2,32 +2,47 @@ import React from "react";
 import Tabs from "../Tabs/";
 import CATEGORIES from "./Categories";
 
-import Emojis from "../Emoji/Emojis";
+import * as EMOJI from "../Emoji/Emojis";
 import EmojiButton from "../Emoji/Button";
+import { toId } from "../utils";
+
+const renderEmojiBtn = ([emoji, names]) => (
+  <EmojiButton key={emoji} emoji={emoji} aria-label={names.join(" ")} />
+);
 
 export default function CategoryTabpanels(props) {
   return (
     <div>
-      <Tabs.Tabpanel id={CATEGORIES.ALL} data-emoji-list>
-        {Emojis.map(emoji => (
-          <EmojiButton
-            key={emoji.e}
-            emoji={emoji.e}
-            aria-label={emoji.n.join(" ")}
-          />
-        ))}
+      <Tabs.Tabpanel id={toId(CATEGORIES.ALL)}>
+        {EMOJI.ALL.map(renderEmojiBtn)}
       </Tabs.Tabpanel>
-      <Tabs.Tabpanel id={CATEGORIES.FACES}>Faces content</Tabs.Tabpanel>
-      <Tabs.Tabpanel id={CATEGORIES.ANIMALS}>Animals content</Tabs.Tabpanel>
-      <Tabs.Tabpanel id={CATEGORIES.FOODS}>Foods content</Tabs.Tabpanel>
-      <Tabs.Tabpanel id={CATEGORIES.SPORTS}>Sports content</Tabs.Tabpanel>
-      <Tabs.Tabpanel id={CATEGORIES.VEHICLES}>Vehicles content</Tabs.Tabpanel>
-      <Tabs.Tabpanel id={CATEGORIES.THINGS}>Things content</Tabs.Tabpanel>
-      <Tabs.Tabpanel id={CATEGORIES.SYMBOLS}>Symbols content</Tabs.Tabpanel>
-      <Tabs.Tabpanel id={CATEGORIES.FLAGS}>Flags content</Tabs.Tabpanel>
-      <Tabs.Tabpanel id={CATEGORIES.SPECIAL}>
-        Special chars content
+      <Tabs.Tabpanel id={toId(CATEGORIES.FACES)}>
+        {EMOJI.FACES_AND_PEOPLE.map(renderEmojiBtn)}
       </Tabs.Tabpanel>
+      <Tabs.Tabpanel id={toId(CATEGORIES.ANIMALS)}>
+        {EMOJI.ANIMALS.map(renderEmojiBtn)}
+      </Tabs.Tabpanel>
+      <Tabs.Tabpanel id={toId(CATEGORIES.FOODS)}>
+        {EMOJI.FOOD_AND_DRINK.map(renderEmojiBtn)}
+      </Tabs.Tabpanel>
+      <Tabs.Tabpanel id={toId(CATEGORIES.ACTIVITIES)}>
+        {EMOJI.ACTIVITIES.map(renderEmojiBtn)}
+      </Tabs.Tabpanel>
+      <Tabs.Tabpanel id={toId(CATEGORIES.TRAVEL_AND_PLACES)}>
+        {EMOJI.TRAVEL_AND_PLACES.map(renderEmojiBtn)}
+      </Tabs.Tabpanel>
+      <Tabs.Tabpanel id={toId(CATEGORIES.OBJECTS)}>
+        {EMOJI.OBJECTS.map(renderEmojiBtn)}
+      </Tabs.Tabpanel>
+      <Tabs.Tabpanel id={toId(CATEGORIES.SYMBOLS)}>
+        {EMOJI.SYMBOLS.map(renderEmojiBtn)}
+      </Tabs.Tabpanel>
+      <Tabs.Tabpanel id={toId(CATEGORIES.FLAGS)}>
+        {EMOJI.FLAGS.map(renderEmojiBtn)}
+      </Tabs.Tabpanel>
+      {/* <Tabs.Tabpanel id={toId(CATEGORIES.NATURE)}>
+        {EMOJI.N.map(renderEmojiBtn)}
+      </Tabs.Tabpanel> */}
     </div>
   );
 }
