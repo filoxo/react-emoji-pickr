8c13be4e2cecbb03d4df0305980f9a0bf84b0eb7
Carlos Filoteo <c.filoteo12@gmail.com>
2020-01-31 08:02:44 -0700

    Handle emoji list focus

--- src/Category/CategoryTabpanel.js
+++ src/Category/CategoryTabpanel.js
@@ -1,9 +1,43 @@
 import React from "react";
 import Tabs from "../Tabs/";
-import EmojiButton from "../Emoji/Button";
 import { toId } from "../utils";
 
-export default function CategoryTabpanels({ category, emoji, onClick }) {
+export default function CategoryTabpanel({ category, emoji, onClick }) {
+  const handleEmojiClick = e => {
+    if (e.target.hasAttribute("data-emoji-button")) {
+      e.target.value = e.target.innerText;
+      onClick(e);
+    }
+  };
+  const getDelta = key => {
+    // TODO: find a better API to allow users to control this
+    switch (key) {
+      case "ArrowUp":
+        return -7;
+      case "ArrowDown":
+        return 7;
+      case "ArrowLeft":
+        return -1;
+      case "ArrowRight":
+        return 1;
+      default:
+        return;
+    }
+  };
+  const handleEmojiNavigation = e => {
+    const delta = getDelta(e.key);
+    if (delta !== undefined) {
+      const emojiIndex = parseInt(e.target.dataset.emojiListIndex, 10);
+      const nextEmojiIndex = emojiIndex + delta;
+      const nextEmoji = document.querySelector(
+        `[data-emoji-list-index="${nextEmojiIndex}"]`
+      );
+      if (nextEmoji) {
+        e.preventDefault();
+        nextEmoji.focus();
+      }
+    }
+  };
   return (
     <Tabs.Tabpanel id={toId(category)}>
       <input
@@ -13,13 +47,20 @@ export default function CategoryTabpanels({ category, emoji, onClick }) {
       />
       <div
         data-emoji-scroll-list
-        onClick={e => {
-          e.target.value = e.target.innerText;
-          onClick(e);
-        }}
+        data-emoji-width-count="6"
+        onClick={handleEmojiClick}
+        onKeyDown={handleEmojiNavigation}
       >
-        {emoji.map(([emoji, names]) => (
-          <EmojiButton key={emoji} emoji={emoji} aria-label={names.join(" ")} />
+        {emoji.map(([emoji, names], index) => (
+          <button
+            key={emoji}
+            type="button"
+            data-emoji-button
+            data-emoji-list-index={index}
+            aria-label={names.join(" ")}
+          >
+            {emoji}
+          </button>
         ))}
       </div>
     </Tabs.Tabpanel>
