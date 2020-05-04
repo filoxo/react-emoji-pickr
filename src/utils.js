58bca2ab4a838172b47e264bc5eb51aafc72643b
Carlos Filoteo <c.filoteo12@gmail.com>
2020-05-04 12:45:57 -0600

    Add getNthSibling util

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,16 @@
 export const toId = (str) => str.replace(/\W/g, "_");
 
 export const toPx = (v) => `${v}px`;
+
+export const getNthSibling = (elem, index) => {
+  if (!elem || index === 0) return elem;
+  if (Math.sign(index) === 1) {
+    return index > 1
+      ? getNthSibling(elem.nextElementSibling, index - 1)
+      : elem.nextElementSibling;
+  } else {
+    return index < -1
+      ? getNthSibling(elem.previousElementSibling, index + 1)
+      : elem.previousElementSibling;
+  }
+};
