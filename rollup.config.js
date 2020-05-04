be09a0aa8b331f6841164316460dfe6968581a8a
Carlos Filoteo <c.filoteo12@gmail.com>
2020-05-03 22:54:47 -0600

    Add commonjs support to Rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import pkg from "./package.json";
 import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import babel from "rollup-plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import serve from "rollup-plugin-serve";
@@ -34,6 +35,7 @@ export default [
       babel({
         exclude: "node_modules/**"
       }),
+      commonjs(),
       postcss({
         extract: true
       }),
