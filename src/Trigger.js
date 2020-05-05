fae38836737add6285202eeb0fd416c36bdc781b
Carlos Filoteo <c.filoteo12@gmail.com>
2020-05-04 22:03:46 -0600

    Add Trigger displayName

--- src/Trigger.js
+++ src/Trigger.js
@@ -3,3 +3,5 @@ import React from "react";
 export const Trigger = React.forwardRef(function Trigger(props, ref) {
   return <button {...props} ref={ref} type="button" data-emoji-trigger />;
 });
+
+Trigger.displayName = "ReactEmojiPickr.Trigger";
